47a21a7409f2258848b4a5c99366e58fcf2787ec
Carl Hu <carl.hu@afusion.com>
2014-11-04 12:14:08 +0800

    111

--- XMPPFramework/Extensions/ChatRoom/XMPPChatRoom.m
+++ XMPPFramework/Extensions/ChatRoom/XMPPChatRoom.m
@@ -12,7 +12,6 @@
 #import "XMPPFramework.h"
 #import "DDList.h"
 
-
 #if ! __has_feature(objc_arc)
 #warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
 #endif
