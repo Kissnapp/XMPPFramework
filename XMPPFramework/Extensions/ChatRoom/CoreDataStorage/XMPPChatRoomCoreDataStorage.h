fffa2c4f097290620eebc7c15dd94d4cb8fa45b0
Peter Lee <peter.lee@afusion.com>
2014-11-06 11:49:34 +0800

    do nothing

--- XMPPFramework/Extensions/ChatRoom/CoreDataStorage/XMPPChatRoomCoreDataStorage.h
+++ XMPPFramework/Extensions/ChatRoom/CoreDataStorage/XMPPChatRoomCoreDataStorage.h
@@ -14,6 +14,7 @@
 #import "XMPPChatRoomUserCoreDataStorageObject.h"
 
 @protocol XMPPChatRoomStorage;
+
 @interface XMPPChatRoomCoreDataStorage : XMPPCoreDataStorage<XMPPChatRoomStorage>
 {
     NSMutableSet *chatRoomPopulationSet;
