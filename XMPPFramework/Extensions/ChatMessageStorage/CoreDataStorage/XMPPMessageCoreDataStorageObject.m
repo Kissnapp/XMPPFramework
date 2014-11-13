406d4136e992d0bda8f4b8edde510a68c53ad933
Peter Lee <peter.lee@afusion.com>
2014-11-13 09:02:30 +0800

    delete some error code

--- XMPPFramework/Extensions/ChatMessageStorage/CoreDataStorage/XMPPMessageCoreDataStorageObject.m
+++ XMPPFramework/Extensions/ChatMessageStorage/CoreDataStorage/XMPPMessageCoreDataStorageObject.m
@@ -20,8 +20,8 @@
 @dynamic messageTime;
 @dynamic sendFromMe;
 @dynamic streamBareJidStr;
+
 //This the getter and setters
-#pragma mark -
 #pragma mark - Getters/Setters methods
 - (NSString *)bareJidStr
 {
